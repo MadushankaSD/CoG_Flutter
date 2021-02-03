8299a99cfb19f57877e951aef0057c18505f7272
madushanka <shanka.sd7@gmail.com>
2021-02-03 15:48:33 +0530

    dummy created

--- lib/blocs/dummy_trip_block.dart
+++ lib/blocs/dummy_trip_block.dart
@@ -3,37 +3,38 @@ import 'package:cloudofgoods/models/trip_model.dart';
 import 'package:cloudofgoods/resources/repository.dart';
 import 'package:rxdart/rxdart.dart';
 
-class TripBlock{
 
+
+final tripBlock = TripBlock();
+
+class TripBlock {
   final _repository = Repository();
   final _tripFetcher = BehaviorSubject<List<AvaliableTrip>>();
   List<AvaliableTrip> _myTrip = [];
+
   // final _albumFetcher = ReplaySubject<ListAlbumModel>();
 
-  dispose(){
+  dispose() {
     _tripFetcher.close();
   }
+
   Stream<List<AvaliableTrip>> get allTrip => _tripFetcher.stream;
 
-  addToMyTrip(AvaliableTrip value){
-    _myTrip.add(value);
-    print(_myTrip);
-  }
+    addToMyTrip(AvaliableTrip value) {
+      _myTrip.add(value);
+      print(_myTrip);
+    }
+    fetchAllTrips() async {
+      List<AvaliableTrip> _tripList = await _repository.getTrips();
+      _tripFetcher.sink.add(_tripList);
+      // for (AvaliableTrip trip in _tripList) {
+      //   _tripFetcher.sink.add(trip);
+      // }
+    }
 
-  removeFromMyTrip(AvaliableTrip value){
-    _myTrip.remove(value);
-    print(_myTrip);
-  }
 
-  fetchAllTrips() async {
-    List<AvaliableTrip> _tripList = await _repository.getTrips();
-    _tripFetcher.sink.add(_tripList);
-    // for (AvaliableTrip trip in _tripList) {
-    //   _tripFetcher.sink.add(trip);
-    // }
-  }
-
-  TripBlock();
+    void removeFromMyTrip(AvaliableTrip value) {
+      _myTrip.remove(value);
+      print(_myTrip);
+    }
 }
-
-final tripBlock = TripBlock();
